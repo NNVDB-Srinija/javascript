16cefce84d7594d0e8624f4be75815453b846b4e
NNVDB-Srinija <155912018+NNVDB-Srinija@users.noreply.github.com>
2025-06-08 08:14:03 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("hello")  //we are using nodejs, not browser.
+
+console.log(3+3)
+console.log("Srinija")
+
+//Readability is more important and prioritized in code
+
+let name="Srinija"  //String
+let age=18   //number
+let isLogged=false
+
+//number ==>till 2^53
+//bigInt
+//String  ==> ""
+//boolean  ===>true/false
+//null   is a stand-alone value, It is empty
+//undefined  --->when value is not defined/initialized. Value not assigned
+//symbol   --->To find uniqueness,  unique
+
+
+//object
+
+console.log(typeof"Srinija")
+console.log(typeof null)
+console.log(typeof age);
+
+
+//typeof ---> To find the datatype of a variable or the following 
