a1f4e8db4fc18e1efe53621ac030cbfaedb448c0
NNVDB-Srinija <155912018+NNVDB-Srinija@users.noreply.github.com>
2025-06-08 16:30:07 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score=false
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueinNumber=Number(score)
+console.log(typeof valueinNumber)
+console.log(valueinNumber);
+
+// "33"  ==> 33
+//"33abc"  ==>NaN
+//true  ==> 1     false ==>0
+//null  ==>0
+//undefined   ===> NaN
+
+let isLogIn="sri"
+
+let booleanLogIn  = Boolean(isLogIn)
+console.log(booleanLogIn)
+
+// 1  ==>true   0 ==>false
+//"" (empty string)==> false
+//"Srinija"  ==>true
+
+let sNum=43
+
+let stringNum=String(sNum)
+console.log(stringNum)
+console.log(typeof stringNum)
