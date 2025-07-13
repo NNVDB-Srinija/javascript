c0ab03d9bb6908a5197075a11f31b240000f8ee4
NNVDB-Srinija <155912018+NNVDB-Srinija@users.noreply.github.com>
2025-07-13 20:03:47 +0000

    Functions continued

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -62,3 +62,53 @@ function defloginUserMessage(username="Sam")
 
 console.log(defloginUserMessage('Srinija'));
 console.log(defloginUserMessage())  
+
+
+// What if we don not know the no of parameters we need to take for example in an ecommerce app we do not know how many products the customers add then
+
+function calculateCartPrice(...num1)
+{
+    return num1
+}
+
+console.log(calculateCartPrice(2,3,4))
+
+// ...(Three dots)  -- spread operator/ rest operator called according to its use in that context
+//we can pass multiple values using rest operator
+
+function calculateCartPrice1(val1,val2,...num1)
+{
+    return num1
+}
+
+console.log(calculateCartPrice1(2,3,4,9,95))
+//Prints only 4,9,95 because the values are stored in val1 and val2 and we are not returning those The parameters are passed in order
+
+const user={
+    username:"hi",
+    price:876
+}
+
+function handleObject(anyObject)
+{
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+handleObject(user)
+
+//If there is a difference in variables it gives undefined in the place of that variable
+//This is solved in TypeScript
+
+handleObject({
+    username:"Sam",
+    price:9374
+})
+
+const newArray=[200,2394,9473,398]
+function returnSecondValue(getArray)
+{
+    return getArray[1]
+}
+
+console.log(returnSecondValue(newArray))
+console.log(returnSecondValue([45,35,34,22]))
